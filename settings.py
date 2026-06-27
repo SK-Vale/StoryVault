97245abbe63a619edd81f831603fb3307c2f3724
SK-Vale <SKValeAuthor@hotmail.com>
2026-06-27 18:19:17 +1000

    Add writing utilities and settings module

---
+++ settings.py
@@ -0,0 +1,6 @@
+settings = {
+    "Username": "Writer",
+    "Theme": "Classic",
+    "Default Goal": 500,
+    "Version": "1.2.0"
+}
---
+++ utils.py
@@ -0,0 +1,11 @@
+def print_header(title):
+
+    print("=" * 30)
+    print(title)
+    print("=" * 30)
+    print()
+
+
+def pause():
+
+    input("Press Enter...")
